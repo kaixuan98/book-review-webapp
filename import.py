16698ea3535a0863926480cec2ff65e069e7d43a
kaixuan98 <kaixuan98@.github.com>
2020-07-12 23:46:07 -0400

    added function to import
    added function to import CSV

--- import.py
+++ import.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import IntegrityError
 engine = create_engine(os.getenv("DATABASE_URL")) 
 db = scoped_session(sessionmaker(bind=engine))
 
-def main():
+def add_author():
     f = open('books.csv')
     reader = csv.reader(f)
     next(reader)
@@ -22,48 +22,63 @@ def main():
             db.execute("INSERT INTO \"author\" (author_name) VALUES (:author) " , 
             {"author": author})
             print(f"added {author}")
-    # here is adding unqiue year into the year table and 
+    print("finished author")
+    db.commit()
+# here is adding unqiue year into the year table and 
+def add_year():
+    f = open('books.csv')
+    reader = csv.reader(f)
+    next(reader)
     for isbn , title ,author , year in reader: 
+        year_int = int(year , base=10)
         if db.execute("SELECT year_value FROM \"year\" WHERE year_value = :year" , 
                         {"year" : year}).rowcount > 0: 
-            next(reader)
+            next(reader , '-1')
+            print(f"-------------------------->already added {year_int}")
         else:
-            year_int = int(year , base=10)
-            db.execute("INSERT INTO \"year\" (year_values) VALUES (:year) " , 
+            db.execute("INSERT INTO \"year\" (year_value) VALUES (:year) " , 
                             {"year":year_int})
             print(f"added {year_int}")
             
-    print("finished")
+    print("finished year")
+    db.commit()
+
+
+def add_books():
+    count = 0 ; 
+    f = open('books.csv')
+    reader = csv.reader(f)
+    next(reader)
     
-        #year_int = int(year , base=10)
+    for isbn , title , author , year in reader: 
+        author_count = db.execute("SELECT id FROM \"author\" WHERE author_name = :author" , 
+                                    {"author" : author}).rowcount
+        year_int = int(year , base=10)
+        year_count = db.execute("SELECT id FROM \"year\" WHERE year_value = :year" , 
+                                {"year" : year_int}).rowcount
+        if author_count == 1 and year_count == 1 : 
+            author_id = db.execute("SELECT * FROM \"author\" WHERE author_name = :author" , 
+                                        {"author" : author}).fetchone()
+            year_id = db.execute("SELECT * FROM \"year\" WHERE year_value = :year" , 
+                                        {"year" : year_int}).fetchone()
+            db.execute("INSERT INTO \"books\" (isbn , title , author_id , year_id ) VALUES (:isbn , :title , :author_id , :year_id)" , 
+                   {"isbn" : isbn , "title": title , "author_id" : author_id.id, "year_id" : year_id.id })
+            print(f"added {isbn} , {title} , {author_id.id} , {year_id.id}")
+        else:
+            count += 1 ; 
+            print(f"did not add ------------------------------ {isbn} , {title} , {author_id.id} , {year_id.id}")
+    
+    print(f"count : {count}")
+
+        
         
 
-    #     db.execute("INSERT INTO \"year\" (year_value) VALUES (:year)", 
-    #         {"year": year_int })
-    #     db.execute("INSERT INTO \"books\" (isbn , title) VALUES (:isbn , :title)" , 
-    #         {"isbn": isbn , "title" : title })
-    #     db.commit()
-
-    # author_ids = db.execute("SELECT * FROM \"author\" ").fetchall()
-
-    # for author in reader: 
-    #     for author_id , author_name in author_ids: 
-    #         if author == author_name:
-    #             db.execute("INSERT INTO \"books\" (author_id) VALUES(:author_id)", 
-    #                         {"author_id": author_id})
-    #             db.commit()
-
-    # year_list = db.execute("SELECT * FROM \"year\" ").fetchall()
-
-    # for year in reader: 
-    #     for year_id , year_value in year_list:
-    #         if year == year_value:
-    #             db.execute("INSERT INTO \"books\" (year_id) VALUES (:year_id)" , 
-    #                         {"year_id" : year_id})
-    #             db.commit()
 
 if __name__ == "__main__":
-    main()
+    #add_author()
+    #add_year()
+    add_books()
+    
 
 
 
